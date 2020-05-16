ae678b65b06d236b4a4deb31a9177944fb653025
yelamansa <aytymbet.e@kolesa.kz>
2020-05-16 10:53:36 +0600

    Добавил новый активити с формой заполнения данных для калькуляции каза

---
+++ app/src/main/java/kz/qazatracker/QazaCalculationActivity.kt
@@ -0,0 +1,12 @@
+package kz.qazatracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QazaCalculationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qaza_calculation)
+    }
+}
