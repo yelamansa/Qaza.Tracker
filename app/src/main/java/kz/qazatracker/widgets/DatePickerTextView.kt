c8c6566d01ed0a1fa4a79834806070acb6bc2a81
yelamansa <aytymbet.e@kolesa.kz>
2020-06-06 12:26:39 +0600

    Проставил ограничение во времени в выборке даты

--- app/src/main/java/kz/qazatracker/widgets/DatePickerTextView.kt
+++ app/src/main/java/kz/qazatracker/widgets/DatePickerTextView.kt
@@ -28,7 +28,9 @@ class DatePickerTextView(
                 dateAndTime.get(Calendar.YEAR),
                 dateAndTime.get(Calendar.MONTH),
                 dateAndTime.get(Calendar.DAY_OF_MONTH)
-            )
+            ).apply {
+                datePicker.maxDate = System.currentTimeMillis()
+            }
             dateDialog?.show()
         }
     }
