846f226aaff2e6a02172c4b05fab69fe2ac74387
yelamansa <aytymbet.e@kolesa.kz>
2020-09-26 11:58:41 +0600

    Поменял id кнопок

--- app/src/main/java/kz/qazatracker/startscreen/StartScreenActivity.kt
+++ app/src/main/java/kz/qazatracker/startscreen/StartScreenActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.Toast
 import kz.qazatracker.R
 import kz.qazatracker.calculation.presentation.QazaCalculationRouter
 import kz.qazatracker.qaza_input.presentation.QazaInputRouter
@@ -12,7 +11,7 @@ import kz.qazatracker.qaza_input.presentation.QazaInputRouter
 class StartScreenActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var calculateButton: Button
-    private lateinit var helpCalculateButton: Button
+    private lateinit var qazaInputButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,21 +21,21 @@ class StartScreenActivity : AppCompatActivity(), View.OnClickListener {
 
     private fun initButtons() {
         calculateButton = findViewById(R.id.calculate_button)
-        helpCalculateButton = findViewById(R.id.help_calculate_button)
+        qazaInputButton = findViewById(R.id.qaza_input_button)
         calculateButton.setOnClickListener(this)
-        helpCalculateButton.setOnClickListener(this)
+        qazaInputButton.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.calculate_button -> {
                 startActivity(
-                    QazaInputRouter().createIntent(this)
+                    QazaCalculationRouter().createIntent(this)
                 )
             }
-            R.id.help_calculate_button -> {
+            R.id.qaza_input_button -> {
                 startActivity(
-                    QazaCalculationRouter().createIntent(this)
+                   QazaInputRouter().createIntent(this)
                 )
             }
         }
