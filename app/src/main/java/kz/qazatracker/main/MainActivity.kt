0b47946d0d92180fe7e68d70d9b14d2986959391
yelamansa <aytymbet.e@kolesa.kz>
2021-01-14 18:36:00 +0600

    Переход в экран корректировки каза

--- app/src/main/java/kz/qazatracker/main/MainActivity.kt
+++ app/src/main/java/kz/qazatracker/main/MainActivity.kt
@@ -2,12 +2,31 @@ package kz.qazatracker.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
 import kz.qazatracker.R
+import kz.qazatracker.qaza_input.presentation.QazaInputRouter
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Toolbar.OnMenuItemClickListener {
+
+    private lateinit var toolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initToolbar()
+    }
+
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        val intent = QazaInputRouter().createIntent(this)
+        startActivity(intent)
+
+        return true
+    }
+
+    private fun initToolbar() {
+        toolbar = findViewById(R.id.toolbar)
+        toolbar.inflateMenu(R.menu.activity_main_toolbar_menu)
+        toolbar.setOnMenuItemClickListener(this)
     }
 }
