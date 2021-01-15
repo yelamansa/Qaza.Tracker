0c69950fdd547fc47f91371a3da992585a1da47a
yelamansa <aytymbet.e@kolesa.kz>
2021-01-15 20:05:02 +0600

    Добавил прогресс в главный экран

--- app/src/main/java/kz/qazatracker/main/MainActivity.kt
+++ app/src/main/java/kz/qazatracker/main/MainActivity.kt
@@ -3,6 +3,7 @@ package kz.qazatracker.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.ProgressBar
 import androidx.appcompat.widget.Toolbar
 import kz.qazatracker.R
 import kz.qazatracker.qaza_input.presentation.QazaInputRouter
@@ -11,11 +12,13 @@ import kz.qazatracker.qaza_input.presentation.QazaInputState
 class MainActivity : AppCompatActivity(), Toolbar.OnMenuItemClickListener {
 
     private lateinit var toolbar: Toolbar
+    private lateinit var mainProgressBar: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initToolbar()
+        initViews()
     }
 
     override fun onMenuItemClick(item: MenuItem?): Boolean {
@@ -29,5 +32,10 @@ class MainActivity : AppCompatActivity(), Toolbar.OnMenuItemClickListener {
         toolbar = findViewById(R.id.toolbar)
         toolbar.inflateMenu(R.menu.activity_main_toolbar_menu)
         toolbar.setOnMenuItemClickListener(this)
+    }
+
+    private fun initViews() {
+        mainProgressBar = findViewById(R.id.main_progress_bar)
+        mainProgressBar.progress = 6
     }
 }
