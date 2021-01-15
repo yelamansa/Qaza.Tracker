2498d368c53282664c9efe2f96acc343780781e4
yelamansa <aytymbet.e@kolesa.kz>
2021-01-15 19:01:50 +0600

    Убрал возврат из главного экрана

--- app/src/main/java/kz/qazatracker/main/MainRouter.kt
+++ app/src/main/java/kz/qazatracker/main/MainRouter.kt
@@ -7,5 +7,7 @@ class MainRouter {
 
     fun createIntent(context: Context): Intent = Intent(
         context, MainActivity::class.java
-    )
+    ).apply {
+        flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+    }
 }
